7902839175a52c4fd3e132f53ede6eacde40e259
Christoph Deil <Deil.Christoph@gmail.com>
2016-12-07 09:36:28 +0100

    Add ctadc/extern/lupton_rgb.py

---
+++ ctadc/extern/__init__.py
@@ -0,0 +1,9 @@
+"""
+Extern code that we bundle here for convenience.
+
+This is just copied files from elsewhere, they should not be modified here.
+
+- `lupton_rgb.py` -- to make nice RGB images.
+  Will be part of `astropy.visualization` soon.
+  See https://github.com/astropy/astropy/pull/5535
+"""
