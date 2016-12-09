de2514834a032f01614d646d2f4f73ff2b65d4a0
Christoph Deil <Deil.Christoph@gmail.com>
2016-12-09 11:40:21 +0100

    Update to sky model scripts

--- ctadc/sky_model.py
+++ ctadc/sky_model.py
@@ -4,39 +4,75 @@ Make sky model.
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 import logging
+from gammapy.catalog import SourceCatalogGammaCat
 
 __all__ = [
-    'GPSSkyModel',
+    'GPSSkyModelSourcesBright',
+    'GPSSkyModelSourcesFaint',
     'make_all_sky_models',
 ]
 
 log = logging.getLogger(__name__)
 
 
-class GPSSkyModel:
+class SkyModelMixin:
     """
-    GPS (Galactic plane survey) sky model.
+    Helper class for sky models.
+
+    This way of structuring code is called a "mix-in" class in Python.
     """
+    def write_xml(self):
+        xml = self.xml
+        filename = self.filename
+
+        log.info('Writing {}'.format(filename))
+        with open(filename, 'w') as fh:
+            fh.write(xml)
+
+
+class GPSSkyModelSourcesBright(SkyModelMixin):
+    """
+    GPS sky model - bright sources component.
+
+    https://github.com/gammapy/gamma-cat/
+    """
+    filename = 'sky_model/ctadc_skymodel_gps_sources_bright.xml'
 
     def __init__(self):
         pass
 
     def make(self):
+        self.make_xml()
+
+    def make_xml(self):
+        self.xml = '<sources></sources>'  # a dummy
+
+
+class GPSSkyModelSourcesFaint(SkyModelMixin):
+    """
+    GPS sky model - faint sources component.
+
+    """
+    filename = 'sky_model/ctadc_skymodel_gps_sources_faint.xml'
+
+    def __init__(self):
         pass
 
-    def to_xml(self):
-        xml = '<sources></sources>'  # a dummy
-        return xml
+    def make(self):
+        self.make_xml()
+
+    def make_xml(self):
+        self.xml = '<sources></sources>'  # a dummy
+
 
 
 def make_all_sky_models():
     """Make all sky models.
     """
-    gps = GPSSkyModel()
-    gps.make()
-    xml = gps.to_xml()
+    gps_sources_bright = GPSSkyModelSourcesBright()
+    gps_sources_bright.make()
+    gps_sources_bright.write_xml()
 
-    filename = 'sky_model/ctadc_skymodel_gps.xml'
-    log.info('Writing {}'.format(filename))
-    with open(filename, 'w') as fh:
-        fh.write(xml)
+    gps_sources_faint = GPSSkyModelSourcesFaint()
+    gps_sources_faint.make()
+    gps_sources_faint.write_xml()
