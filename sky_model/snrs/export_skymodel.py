b74e291fed90f2b50a11b90f02ce01252f7000e2
Christoph Deil <Deil.Christoph@gmail.com>
2017-02-24 18:41:55 +0100

    Update SNRs for sky model

--- sky_model/snrs/export_skymodel.py
+++ sky_model/snrs/export_skymodel.py
@@ -1,97 +1,98 @@
-##############################################################################################
-############# Jan 2017 - export simulated SNRs for Roberta and Christoph        ##############
-##############################################################################################
-## Export '(Results.txt)'  from .txt to .ecsv 
-
-
-
+"""
+Reformat data for simulated SNRs by Pierre Cristofari from TXT to ECSV
+"""
 import numpy as np
-import matplotlib.pyplot as plt
-import pylab as pl
-# from matplotlib.ticker import ScalarFormatter
-# import matplotlib.ticker as mticker
-# import matplotlib.ticker as ticker
 import astropy.units as u
-
-######################################################################
-# READ ORIGINAL .TXT FILES
-
-data = np.genfromtxt('test_skymodel.txt', defaultfmt='%0.6f', names=True)
-# Trick to read the Energy grid
-data2 = np.genfromtxt('test_skymodel.txt', defaultfmt='%0.6f')
-EGRID = data2[0]
-EGRID = EGRID[~np.isnan(EGRID)]
-size = len(data.dtype.names)
-number_of_SNRs = len(data)  # number of SNRs
-
-######################################################################
-# BUILD ARRAYS BEFORE WRITING IN .ECSV
-test = [None] * number_of_SNRs
-for j in range(0, number_of_SNRs):
-    test[j] = [data[j][0]]
-    for i in range(1, size):
-        test[j] = np.append(test[j], data[j][i])
-
-temp = test[0]
-for j in range(1, number_of_SNRs):
-    temp = np.vstack([temp, test[j]])
-
-NAMES = data.dtype.names[0]
-for x in range(1, 9):
-    NAMES = np.append(NAMES, data.dtype.names[x])
-for x in range(9, size):
-    NAMES = np.append(NAMES, EGRID[x - 9])
-
-######################################################################
-# WRITE IN .ECSV
-
 from astropy.table import Table
 
-t = Table(temp, names=NAMES)
 
-t.meta[
-    'hello'] = 'Parameters used for this simulation: spectral index alpha=4.1; Electron to proton ratio Kep=10-2; CR efficiency xi=0.1'
-t.meta['energy_array'] = EGRID
+def read_txt_files():
+    filename = 'test_skymodel.txt'
+    print('Reading {}'.format(filename))
+    data = np.genfromtxt(filename, defaultfmt='%0.6f', names=True)
 
-t[NAMES[0]].unit = ''
-t[NAMES[0]].format = '%i'
-t[NAMES[0]].description = 'Simulation_number'
+    # Trick to read the Energy grid
+    data2 = np.genfromtxt(filename, defaultfmt='%0.6f')
 
-t[NAMES[1]].unit = 'kpc'
-t[NAMES[1]].format = '%0.4f'
-t[NAMES[1]].description = 'X Position'
+    EGRID = data2[0]
+    EGRID = EGRID[~np.isnan(EGRID)]
+    size = len(data.dtype.names)
+    number_of_SNRs = len(data)  # number of SNRs
 
-t[NAMES[2]].unit = 'kpc'
-t[NAMES[2]].format = '%0.4f'
-t[NAMES[2]].description = 'Y Position'
+    # Reformat spectral data into arrays
+    test = [None] * number_of_SNRs
+    for j in range(0, number_of_SNRs):
+        test[j] = [data[j][0]]
+        for i in range(1, size):
+            test[j] = np.append(test[j], data[j][i])
 
-t[NAMES[3]].unit = 'kpc'
-t[NAMES[3]].format = '%0.4f'
-t[NAMES[3]].description = 'Z Position'
+    temp = test[0]
+    for j in range(1, number_of_SNRs):
+        temp = np.vstack([temp, test[j]])
 
-t[NAMES[4]].unit = u.Unit('cm-3')
-t[NAMES[4]].format = '%0.4f'
-t[NAMES[4]].description = 'density'
+    NAMES = data.dtype.names[0]
+    for x in range(1, 9):
+        NAMES = np.append(NAMES, data.dtype.names[x])
+    for x in range(9, size):
+        NAMES = np.append(NAMES, EGRID[x - 9])
 
-t[NAMES[5]].unit = ''
-t[NAMES[5]].format = '%i'
-t[NAMES[5]].description = 'type of progenitor'
+    t = Table(temp, names=NAMES)
 
-t[NAMES[6]].unit = 'kyear'
-t[NAMES[6]].format = '%0.4f'
-t[NAMES[6]].description = 'age of the SNR'
+    # Parameters used for this simulation
+    t.meta['simulation_parameters'] = [
+        dict(name='alpha', value=4.1, description='Spectral index'),
+        dict(name='Kep', value=1e-2, description='Electron to proton ratio'),
+        dict(name='xi', value=0.1, description='CR efficiency'),
+    ]
 
-t[NAMES[7]].unit = 'arcmin'
-t[NAMES[7]].format = '%0.4f'
-t[NAMES[7]].description = 'apparent size of the SNR'
+    # TODO: add this back as Python list!
+    # t.meta['energy_array'] = EGRID
 
-t[NAMES[8]].unit = 'pc'
-t[NAMES[8]].format = '%0.4f'
-t[NAMES[8]].description = 'SNR Radius'
+    t[NAMES[0]].unit = ''
+    t[NAMES[0]].format = '%i'
+    t[NAMES[0]].description = 'Simulation_number'
 
-for x in range(9, size):
-    t[NAMES[x]].unit = 'TeV^{-1}cm^{-2}s^{-1}'
-    t[NAMES[x]].description = 'Differential spectrum'
+    t[NAMES[1]].unit = 'kpc'
+    t[NAMES[1]].format = '%0.4f'
+    t[NAMES[1]].description = 'X Position'
 
-print(t)
-t.write('SNRs_SIMULATED_SKYMODEL_TEST.ecsv', format='ascii.ecsv')
+    t[NAMES[2]].unit = 'kpc'
+    t[NAMES[2]].format = '%0.4f'
+    t[NAMES[2]].description = 'Y Position'
+
+    t[NAMES[3]].unit = 'kpc'
+    t[NAMES[3]].format = '%0.4f'
+    t[NAMES[3]].description = 'Z Position'
+
+    t[NAMES[4]].unit = u.Unit('cm-3')
+    t[NAMES[4]].format = '%0.4f'
+    t[NAMES[4]].description = 'density'
+
+    t[NAMES[5]].unit = ''
+    t[NAMES[5]].format = '%i'
+    t[NAMES[5]].description = 'type of progenitor'
+
+    t[NAMES[6]].unit = 'kyear'
+    t[NAMES[6]].format = '%0.4f'
+    t[NAMES[6]].description = 'age of the SNR'
+
+    t[NAMES[7]].unit = 'arcmin'
+    t[NAMES[7]].format = '%0.4f'
+    t[NAMES[7]].description = 'apparent size of the SNR'
+
+    t[NAMES[8]].unit = 'pc'
+    t[NAMES[8]].format = '%0.4f'
+    t[NAMES[8]].description = 'SNR Radius'
+
+    for x in range(9, size):
+        t[NAMES[x]].unit = 'TeV^{-1}cm^{-2}s^{-1}'
+        t[NAMES[x]].description = 'Differential spectrum'
+
+    return t
+
+
+if __name__ == '__main__':
+    table = read_txt_files()
+    filename = 'ctadc_skymodel_gps_sources_snr.ecsv'
+    print('Writing {}'.format(filename))
+    table.write(filename, format='ascii.ecsv', overwrite=True)
--- sky_model/snrs/snrs_to_xml.py
+++ sky_model/snrs/snrs_to_xml.py
@@ -1,9 +1,11 @@
 """
 Convert SNRs to XML format.
 """
+import numpy as np
+import astropy.units as u
 from astropy.table import Table
 
-SOURCE_LIBRARY_TEMPLATE = """
+SOURCE_LIBRARY_TEMPLATE = """\
 <?xml version="1.0" standalone="no"?>
 
 <source_library title="CTA 1DC simulated supernova remnants">
@@ -14,34 +16,79 @@ SOURCE_LIBRARY_TEMPLATE = """
 """
 
 SOURCE_TEMPLATE = """
- <source name="{source_name}" type="PointSource">
-    <spectrum type="PowerLaw">
-        <parameter name="Prefactor" value="6.500000190867716e-13" unit="1 / (cm2 s TeV)"/>
-        <parameter name="Index" value="-2.6500000953674316" unit=""/>
-        <parameter name="Scale" value="1.0" unit="TeV"/>
-    </spectrum>
-    <spatialModel type="Gauss">
-        <parameter name="GLON" value="{glon}" unit="deg"/>
-        <parameter name="GLAT" value="{glat}" unit="deg"/>
-        <parameter name="Sigma" value="{sigma}" unit="deg"/>
-    </spatialModel>
+ <source name="{source_name}" type="ExtendedSource">
+{xml_spectral}
+{xml_spatial}
  </source>
 """
 
+SPATIAL_TEMPLATE = """\
+    <spatialModel type="RadialShell">
+        <parameter name="GLON" value="{glon:.5f}" unit="deg"/>
+        <parameter name="GLAT" value="{glat:.5f}" unit="deg"/>
+        <parameter name="Radius" value="{radius:.5f}" unit="deg"/>
+        <parameter name="Width" value="{width:.5f}" unit="deg"/>
+    </spatialModel>"""
 
-def make_snr_xml():
-    filename = 'SNRs_SIMULATED_SKYMODEL_TEST.ecsv'
-    table = Table.read(filename, format='ascii.ecsv')
+SPECTRUM_TEMPLATE = """\
+    <spectrum type="NodeFunction">
+{xml_spectrum_nodes}\
+    </spectrum>"""
+
+SPECTRUM_NODE_TEMPLATE = """\
+      <node>
+        <parameter name="Energy" value="{energy:.5f}" unit="TeV"/>
+        <parameter name="Intensity" value="{dnde:.5g}" unit="cm-2 s-1 TeV-1"/>
+      </node>
+"""
+
+
+def make_table_spectrum_xml(energy_list, dnde_list):
+    xml_spectrum_nodes = ''
+    for energy, dnde in zip(energy_list, dnde_list):
+        xml_spectrum_nodes += SPECTRUM_NODE_TEMPLATE.format(
+            energy=energy,
+            dnde=dnde,
+        )
+
+    return SPECTRUM_TEMPLATE.format(xml_spectrum_nodes=xml_spectrum_nodes)
+
+
+def make_snr_xml(data):
+    table = data['table']
+    # print(table.colnames)
+    # table.info()
+    # table.info('stats')
 
     xml_sources = ''
-    for idx in [1, 2, 3]:
-        pars = dict(
-            source_name='snr_{}'.format(idx),
+    for row in table[:3]:
+
+        xml_spectral = make_table_spectrum_xml(
+            energy_list=data['energy'],
+            dnde_list=data['dnde'],
+        )
+
+        # Arbitrary assumption on width of the SNR shell
+        # TODO: ask Pierre what we should put!
+        # Also check if radius is inner or outer!
+        width_fraction = 0
+        radius = u.Quantity(row['size'], 'arcmin').to('deg')
+        width = width_fraction * radius
+        xml_spatial = SPATIAL_TEMPLATE.format(
             glon=42,
             glat=43,
-            sigma=99,
+            radius=radius.value,
+            width=width.value,
         )
-        xml_sources += SOURCE_TEMPLATE.format(**pars)
+
+        source_name = 'snr_{}'.format(row.index)
+        xml_source = SOURCE_TEMPLATE.format(
+            source_name=source_name,
+            xml_spectral=xml_spectral,
+            xml_spatial=xml_spatial,
+        )
+
+        xml_sources += xml_source
 
     xml = SOURCE_LIBRARY_TEMPLATE.format(xml_sources=xml_sources)
 
@@ -51,5 +98,17 @@ def make_snr_xml():
         fh.write(xml)
 
 
+def read_snr_data():
+    filename = 'ctadc_skymodel_gps_sources_snr.ecsv'
+    table = Table.read(filename, format='ascii.ecsv')
+    data = dict(
+        table=table,
+        energy=np.linspace(1, 3, 5),
+        dnde=np.logspace(-10, -8, 5),
+    )
+    return data
+
+
 if __name__ == '__main__':
-    make_snr_xml()
+    data = read_snr_data()
+    make_snr_xml(data)
