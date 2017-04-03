469866ac14ef194ba3587e94954c1225a8a9b089
robertazanin <Roberta.Zanin@mpi-hd.mpg.de>
2017-04-03 21:25:48 +0200

    update logNlogS

---
+++ sky_model/snrs/plot_logN_logS_snr.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+from snr_to_xml import read_snr_data
+
+
+
+def plot_logN_logS:
+
+    table = read_snr_data(2)
+    table.pprint()
+
+
--- sky_model_checks/plot_logN_logS_gammalib.py
+++ sky_model_checks/plot_logN_logS_gammalib.py
@@ -22,6 +22,7 @@ def compute_fluxes(filename):
         flux_cu = (flux/crab_integral_flux_1to10TeV)*100
         fluxes.append(flux)
         fluxes_cu.append(flux_cu)
+        print(name, model.spectral(), flux, flux_cu, crab_integral_flux_1to10TeV)
     return names, fluxes, fluxes_cu
 
 
@@ -35,11 +36,13 @@ def make_plot(num_plot, color_scale, fluxes_cu,fluxes_cu_2,fluxes_cu_3,fluxes_cu
     bins = np.logspace(np.log10(flux_min), np.log10(flux_max), num_bins)
     hist, bins_edges = np.histogram(fluxes_cu, bins=bins)
     y = np.insert(hist[::-1].astype('float64').cumsum(), 0, 0.01)
+
     plt.step(bins[::-1], y, color=color_scale[0], lw=2)
     if num_plot > 1:
         hist_2, bins_edges_2 = np.histogram(fluxes_cu_2, bins=bins)
         y_2 = np.insert(hist_2[::-1].astype('float64').cumsum(), 0, 0.01)
         plt.step(bins[::-1], y_2, color=color_scale[1], lw=2)
+
     if num_plot > 2:
         hist_3, bins_edges_3 = np.histogram(fluxes_cu_3, bins=bins)
         y_3 = np.insert(hist_3[::-1].astype('float64').cumsum(), 0, 0.01)
@@ -53,8 +56,28 @@ def make_plot(num_plot, color_scale, fluxes_cu,fluxes_cu_2,fluxes_cu_3,fluxes_cu
         y_5 = np.insert(hist_5[::-1].astype('float64').cumsum(), 0, 0.01)
         plt.step(bins[::-1], y_5, color=color_scale[4], lw=2)
 
+
+
+
+   # import IPython; IPython.embed();
+    y_sum = []
+    y_bright = []
+
+    for ii in range(len(y)):
+         ysum = y[ii]+y_2[ii]#+y_3[ii]+y_4[ii]+y_5[ii]
+         #ybright = y_4[ii] +y_5[ii]
+         y_sum.append(ysum)
+         #y_bright.append(ybright)
+         # print(y[ii], y_2[ii], y_3[ii], y_4[ii], y_5[ii], y_sum[ii], y_bright[ii])
+         print(y[ii], y_2[ii], y_sum[ii])
+
+
+
+    #hist_sum, bin_edges_sum = np.histogram(y_sum,bins=bins)
+    plt.step(bins[::-1], y_sum, color=color_scale[5], lw=2)
+
     plt.loglog()
-    plt.ylim(0.8, 5000)
+    plt.ylim(0.8, 2000)
     plt.xlim(0.05, 100)
     plt.ylabel('#')
     plt.xlabel('F (1-10 TeV) [cu]')
@@ -75,22 +98,24 @@ def make_plot(num_plot, color_scale, fluxes_cu,fluxes_cu_2,fluxes_cu_3,fluxes_cu
 
 
 if __name__ == '__main__':
-    num_plot = 5
-    color_scale = ['red','blue','green','magenta','black']
+    num_plot = 2
+    color_scale = ['red','blue','green','magenta','yellow','black']
 
     filename_pwn = '../sky_model/pwn/ctadc_skymodel_gps_sources_pwn.xml'
-    names_pwn, fluxes_pwn, fluxes_cu_pwn = compute_fluxes(filename_pwn)
+    #names_pwn, fluxes_pwn, fluxes_cu_pwn = compute_fluxes(filename_pwn)
 
     filename_binaries = '../sky_model/binaries/ctadc_skymodel_gps_sources_binaries.xml'
-    names_binaries, fluxes_binaries, fluxes_cu_binaries = compute_fluxes(filename_binaries)
+    #names_binaries, fluxes_binaries, fluxes_cu_binaries = compute_fluxes(filename_binaries)
 
     filename_snr = '../sky_model/snrs/ctadc_skymodel_gps_sources_snr_2.xml'
-    names_snr, fluxes_snr, fluxes_cu_snr = compute_fluxes(filename_snr)
+    #names_snr, fluxes_snr, fluxes_cu_snr = compute_fluxes(filename_snr)
 
-    filename_gammacat = '../sky_model/gamma-cat/ctadc_skymodel_gps_sources_gamma-cat.xml'
-    #names_gammacat, fluxes_gammacat, fluxes_cu_gammacat = compute_fluxes(filename_gammacat)
+    filename_gammacat = '../sky_model/gamma-cat/ctadc_skymodel_gps_sources_gamma-cat2.xml'
+    names_gammacat, fluxes_gammacat, fluxes_cu_gammacat = compute_fluxes(filename_gammacat)
+
+    print(len(fluxes_cu_gammacat))
 
     filename_images = '../sky_model/image_sources/ctadc_skymodel_gps_sources_images.xml'
     names_images, fluxes_images, fluxes_cu_images = compute_fluxes(filename_images)
 
-    make_plot(num_plot, color_scale, fluxes_cu_pwn, fluxes_cu_snr, fluxes_cu_binaries, fluxes_cu_images, fluxes_cu_pwn)
+    make_plot(num_plot, color_scale, fluxes_cu_gammacat, fluxes_cu_images, fluxes_cu_images, fluxes_cu_images, fluxes_cu_gammacat)
