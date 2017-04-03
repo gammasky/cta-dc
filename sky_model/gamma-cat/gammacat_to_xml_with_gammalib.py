c3b63cfb4305d8bc3610a961352a3fd0204b8a0f
Christoph Deil <Deil.Christoph@gmail.com>
2017-04-03 08:08:27 +0200

    update

--- sky_model/gamma-cat/gammacat_to_xml_with_gammalib.py
+++ sky_model/gamma-cat/gammacat_to_xml_with_gammalib.py
@@ -3,8 +3,13 @@ Convert Gamma-cat to model XML format.
 
 Use Gammalib for now to make sure the XML format is OK.
 """
+import logging
 import gammalib
 from gammapy.catalog import SourceCatalogGammaCat
+
+logging.basicConfig(level='DEBUG')
+log = logging.getLogger(__name__)
+
 
 def gammacat_source_to_gammalib_model(source):
     # import IPython; IPython.embed(); 1/0
@@ -33,8 +38,8 @@ def gammacat_source_to_gammalib_model_spatial(gammapy_spatial):
         # import IPython; IPython.embed(); 1/0
         # gammalib_spatial = gammalib.GModelSpatialPointSource(pos)
         pos.lb_deg(gammapy_spatial.x_0.value, gammapy_spatial.y_0.value)
-        radius = gammapy_spatial.r_in.value # TODO: is this inner or outer radius?
-        width= gammapy_spatial.width.value
+        radius = gammapy_spatial.r_in.value  # TODO: is this inner or outer radius?
+        width = gammapy_spatial.width.value
         gammalib_spatial = gammalib.GModelSpatialRadialShell(pos, radius, width)
     else:
         raise NotImplementedError('Not supported: {}'.format(kind))
@@ -50,7 +55,6 @@ def gammacat_source_to_gammalib_model_spectral(gammapy_spectral):
         pivot = gammalib.GEnergy(gammapy_spectral.parameters['reference'].quantity.to('MeV').value, 'MeV')
         gammalib_spectral = gammalib.GModelSpectralPlaw(prefactor, index, pivot)
     elif kind == 'PowerLaw2':
-        # import IPython; IPython.embed(); 1/0
         flux = gammapy_spectral.parameters['amplitude'].quantity.to('cm-2 s-1').value
         index = gammapy_spectral.parameters['index'].value
         emin = gammalib.GEnergy(gammapy_spectral.parameters['emin'].quantity.to('MeV').value, 'MeV')
@@ -64,11 +68,53 @@ def gammacat_source_to_gammalib_model_spectral(gammapy_spectral):
         gammalib_spectral = gammalib.GModelSpectralExpPlaw(prefactor, index, pivot, cutoff)
     else:
         raise NotImplementedError('Not supported: {}'.format(kind))
+
     return gammalib_spectral
 
 
-def gammacat_to_xml_version1():
+THE_OTHERS = [
+    # Sources in image_sources/ctadc_skymodel_gps_sources_images.xml
+    'Westerlund 1',
+    # 'Puppis A',  # Not a source in gamma-cat anyways
+    'Vela X',
+    'Vela Junior',
+    'RX J1713.7-3946',
+    'W28'  # a.k.a. "HESS J1801-233"
+    'HESS J1800-240A',
+    'HESS J1800-240B',
+    # Sources in binaries/ctadc_skymodel_gps_sources_binaries.xml
+    'LS 5039',
+    'PSR B1259-63',
+    'HESS J1832-093',
+    'LS I +61 303',
+    'HESS J0632+057',
+    'HESS J1018-589 A',
+    # Sources we don't really need
+    'Galactic Centre ridge',  # not a source
+    'SN 1006',  # not in the survey region
+    'Arc source',  # very recent, and faint
+    'Geminga',  # no good TeV measurement available
+
+]
+
+
+def skip_source(source):
+    txt = '{} (gammacat source_id={})'.format(source.name, source.data['source_id'])
+    if source.data['where'] == 'egal':
+        # log.debug('Skipped EGAL source: {}'.format(txt))
+        return True
+
+    if source.name in THE_OTHERS:
+        # log.debug('Skipped source that belongs to THE OTHERS: {}'.format(txt))
+        return True
+
+    # log.debug('Keeping source: {}'.format(txt))
+    return False
+
+
+def gammacat_to_xml_gammalib():
     cat = SourceCatalogGammaCat()
+    log.info('Number of sources in gamma-cat: {}'.format(len(cat.table)))
 
     models = gammalib.GModels()
     for source_idx in range(len(cat.table)):
@@ -76,32 +122,23 @@ def gammacat_to_xml_version1():
         # if source_idx > 5: break
 
         source = cat[source_idx]
-        # print('\n\n*** Source: {} ***\n'.format(source.name))
+        if skip_source(source):
+            continue
+
         try:
             model = gammacat_source_to_gammalib_model(source)
             models.append(model)
-        except ValueError as exc:
-            # import IPython; IPython.embed(); 1/0
-            if source.data['where'] != 'egal':
-                print('Skipped non-EGAL source: {} (gammacat source_id={})'.format(source.name, source.data['source_id']))
-            # print(exc)
-            # raise
+        except ValueError:
+            txt = '{} (gammacat source_id={})'.format(source.name, source.data['source_id'])
+            log.error('MISSING DATA: {}'.format(txt))
+            pass
+
+    log.info('Number of sources in XML file: {}'.format(len(models)))
 
     filename = 'ctadc_skymodel_gps_sources_gamma-cat2.xml'
     print('Writing {}'.format(filename))
     models.save(filename)
 
 
-def gammacat_to_xml_version2():
-    cat = SourceCatalogGammaCat()
-    xml = ''
-
-    for source_idx in range(len(cat.table)):
-        source = cat[source_idx]
-        xml += source_to_xml(source)
-
-    return xml
-
-
 if __name__ == '__main__':
-    gammacat_to_xml_version1()
+    gammacat_to_xml_gammalib()
